0cb78cf2417d2cd604562a8c5de0fc889826a867
meniYud <menyud@gmail.com>
2022-12-17 12:34:20 +0200

    format (bugFix with js vs cjs for prettier)

--- src/App.jsx
+++ src/App.jsx
@@ -23,30 +23,30 @@ const App = () => {
   return (
     <div className="m-0 p-0">
       <QueryClientProvider client={queryClient}>
-          <AdoptedPetContext.Provider value={adoptedPet}>
-            <ErrorBoundary>
-              <Suspense
-                fallback={
-                  <div className="loading-pane">
-                    <h2 className="loader">🐬</h2>
-                  </div>
-                }
-              >
-                <header className="main-header mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center">
-                  <Link
-                    to="/"
-                    className="text-6xl text-white hover:text-gray-200"
-                  >
-                    Playground
-                  </Link>
-                </header>
-                <Routes>
-                  <Route path="/details/:id" element={<Details />} />
-                  <Route path="/" element={<SearchParams />} />
-                </Routes>
-              </Suspense>
-            </ErrorBoundary>
-          </AdoptedPetContext.Provider>
+        <AdoptedPetContext.Provider value={adoptedPet}>
+          <ErrorBoundary>
+            <Suspense
+              fallback={
+                <div className="loading-pane">
+                  <h2 className="loader">🐬</h2>
+                </div>
+              }
+            >
+              <header className="main-header mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center">
+                <Link
+                  to="/"
+                  className="text-6xl text-white hover:text-gray-200"
+                >
+                  Playground
+                </Link>
+              </header>
+              <Routes>
+                <Route path="/details/:id" element={<Details />} />
+                <Route path="/" element={<SearchParams />} />
+              </Routes>
+            </Suspense>
+          </ErrorBoundary>
+        </AdoptedPetContext.Provider>
       </QueryClientProvider>
     </div>
   );
